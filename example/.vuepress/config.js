5b556fb613536964442b5d8071125b5f4aa8197a
ULIVZ <472590061@qq.com>
2019-06-03 21:37:07 +0800

    chore: remove example

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -1,6 +1,22 @@
 module.exports = {
   title: `ULIVZ`,
+  theme: 'blogging',
   themeConfig: {
+    modifyBlogPluginOptions(blogPlugnOptions) {
+      const writingDirectoryClassifier = {
+        id: 'writing',
+        dirname: '_writings',
+        path: '/writings/',
+        layout: 'IndexWriting',
+        itemLayout: 'Writing',
+        itemPermalink: '/writings/:year/:month/:day/:slug',
+        pagination: {
+          perPagePosts: 5,
+        },
+      }
+      blogPlugnOptions.directories.push(writingDirectoryClassifier)
+      return blogPlugnOptions
+    },
     nav: [
       {
         text: 'Blog',
