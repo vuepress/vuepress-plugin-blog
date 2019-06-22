56223805e7d48ecc4553851b70eaab881c3203e6
ULIVZ <472590061@qq.com>
2019-06-22 23:55:32 +0800

    fix($components): SimplePagination doesn't work

--- src/client/components/SimplePagination.vue
+++ src/client/components/SimplePagination.vue
@@ -1,4 +1,6 @@
-<div id="pagination">
-  <router-link v-if="$pagination.hasPrev" :to="$pagination.prevLink">Prev</router-link>
-  <router-link v-if="$pagination.hasNext" :to="$pagination.nextLink">Next</router-link>
-</div>
+<template>
+  <div id="pagination">
+    <router-link v-if="$pagination.hasPrev" :to="$pagination.prevLink">Prev</router-link>
+    <router-link v-if="$pagination.hasNext" :to="$pagination.nextLink">Next</router-link>
+  </div>
+</template>
