303c619f6ec52f34721d392dc99f7fc7351aed6c
ULIVZ <472590061@qq.com>
2019-09-07 00:27:22 +0800

    fix: add mising types (#22)

---
+++ src/types/VuePress.d.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+export declare interface VuePressPage {
+  key: string;
+  regularPath: string;
+  frontmatter: Record<string, string>;
+}
+
+export declare interface VuePressContext {
+  sourceDir: string;
+  pages: VuePressPage[];
+  themeAPI: {
+    layoutComponentMap: Record<string, Vue>
+  };
+  addPage: any;
+}
