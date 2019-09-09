6f914d9f85e17557c7ba5e6ebffc31f944e8c8f8
qiang <qw13131wang@gmail.com>
2019-09-09 10:45:17 +0800

    fix: Pagination Number by computed

--- src/client/components/Pagination.vue
+++ src/client/components/Pagination.vue
@@ -2,7 +2,7 @@
   <component
     v-if="comp"
     :is="comp"
-    v-model="page"
+    :value="page"
     :page-count="$pagination.length"
     :click-handler="clickCallback"
     :prev-text="'Prev'"
@@ -16,21 +16,16 @@
   export default {
     data() {
       return {
-        page: 0,
         comp: null,
       }
     },
 
-    watch: {
-      $pagination() {
-        this.page = this.$pagination.paginationIndex + 1
+    computed: {
+      page() {
+        return this.$pagination.paginationIndex + 1
       }
     },
 
-    created() {
-      this.page = this.$pagination.paginationIndex + 1
-    },
-    
     mounted() {
       import(/* webpackChunkName: "vuejs-paginate" */ 'vuejs-paginate').then(comp => {
         this.comp = comp.default
