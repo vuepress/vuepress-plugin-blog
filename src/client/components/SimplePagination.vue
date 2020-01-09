86290423e412199f376f87ec9de687807739a064
Billyyyyy3320 <newsbielt703@gmail.com>
2020-01-09 23:35:32 +0800

    style: format stylus

--- src/client/components/SimplePagination.vue
+++ src/client/components/SimplePagination.vue
@@ -12,20 +12,21 @@
 <style lang="stylus">
 .simple-pagination
   a
-    margin-right: 20px
-    color: #000
-    height: 38px
-    line-height: 38px
-    transition: all .3s ease
-    position: relative
-    overflow: hidden
-    display: inline-block
+    margin-right 20px
+    color #000
+    height 38px
+    line-height 38px
+    transition all 0.3s ease
+    position relative
+    overflow hidden
+    display inline-block
     background #FFF
-    padding: 0 15px
-    text-decoration: none
+    padding 0 15px
+    text-decoration none
     border 1px solid #000
     border-radius 5px
-    transition all .5s
+    transition all 0.5s
+
     &:hover
       color #FFF
       border 1px solid $accentColor
